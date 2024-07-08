b897fb42aca85ff5f9232997d3fcbeb22e965a70
salvatorebriga <salvo.z2302@gmail.com>
2024-07-08 16:36:45 +0200

    BONUS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,16 +5,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Template</title>
+    <title>Laravel MVC</title>
     @vite('resources/js/app.js')
   </head>
 
   <body>
+    {{-- <img src="{{ Vite::asset('resources/img/duck.jpg') }}" alt="Paperella Laravel Template"> --}}
+
     <h1>
-      Prova
+      Laravel MVC
     </h1>
 
-    <img src="{{ Vite::asset('resources/img/duck.jpg') }}" alt="Paperella Laravel Template">
+    <div class="container">
+      <div class="row">
+        @foreach ($movies as $movie)
+          <div class="col">
+            <div class="card">
+              <p> {{ $movie['title'] }} </p>
+              <p> {{ $movie['original_title'] }} </p>
+              <p> {{ $movie['nationality'] }} </p>
+              <p> {{ $movie['date'] }} </p>
+              <p> {{ $movie['vote'] }} </p>
+            </div>
+          </div>
+        @endforeach
+      </div>
+    </div>
+
 
   </body>
 
