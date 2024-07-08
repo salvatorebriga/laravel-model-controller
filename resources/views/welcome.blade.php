caddcb1705ef70cc264386fb3ee8afa33fe26f47
salvatorebriga <salvo.z2302@gmail.com>
2024-07-08 14:48:21 +0200

    removed bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Template</title>
     @vite('resources/js/app.js')
-</head>
+  </head>
 
-<body>
+  <body>
     <h1>
-        Prova
+      Prova
     </h1>
 
     <img src="{{ Vite::asset('resources/img/duck.jpg') }}" alt="Paperella Laravel Template">
 
-</body>
+  </body>
 
 </html>
